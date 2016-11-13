f9aca2dadae7ff35c6cc82e25632e20d5356b787
Melissa Phillips <melissanicolephillips@gmail.com>
2016-11-13 14:16:00 -0600

    Added View Controller Code

--- future-date-generator/ViewController.swift
+++ future-date-generator/ViewController.swift
@@ -9,17 +9,108 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet var monthTextField: UITextField!
+	@IBOutlet var dayTextField: UITextField!
+	@IBOutlet var yearTextField: UITextField!
+	@IBOutlet var addSecondsTextField: UITextField!
+	@IBOutlet var dateLabel: UILabel!
+	
+	
+	@IBAction func calculateDateButton(_ sender: Any) {
+		
+		// Check if values exist
+		
+		if monthTextField.text != "" && dayTextField.text != "" && yearTextField.text != "" {
+			
+			//Check if value entered for Month is valid
+			
+			switch Int(monthTextField.text!)! {
+				
+				case 1...12:
+				
+				//Check if value entered for Day is valid
+				
+					switch Int(dayTextField.text!)! {
+					
+				case 1...31:
+					
+						//Check if value entered for Year is valid
+						
+					switch Int(yearTextField.text!)! {
+						
+				case 1000..<9999:
+						
+					// Create Date string variable
+							
+					let dateString: String = "\(yearTextField.text!) - \(monthTextField.text!) - \(dayTextField.text!) 12:00:00 AM"
+							
+					let formatter:DateFormatter = DateFormatter()
+					formatter.amSymbol = "AM"
+					formatter.pmSymbol = "PM"
+					formatter.dateFormat = "yyyy-MM-dd hh:mm:ss a"
+						
+					// Add seconds to Date
+							
+					if addSecondsTextField.text == "" {
+							
+					print("Seconds Empty")
+					dateLabel.text = formatter.string(from: formatter.date(from: dateString)!)
+					print(formatter.string(from: formatter.date(from: dateString)!))
+							
+						} else {
+							
+							print("Seconds not Empty")
+							// convert seconds to minutes
+							let secToAdd: Double = Double(addSecondsTextField.text!)! * 60
+							// Add specified time amount to given date
+							let newDate:Date = Date.init(timeInterval: secToAdd, since: formatter.date(from: dateString)!)
+							dateLabel.text = formatter.string(from: newDate)
+						}
+						
+					default:
+						dateLabel.text = "Incorrect Year"
+						return
+					}
+				default:
+					dateLabel.text = "Incorrect Day"
+					return
+				}
+			default:
+				dateLabel.text = "Incorrect Month"
+				return
+			}
+			
+		} else {
+			dateLabel.text = "Missing Date Components"
+		}
+		
+	}
+	
+	@IBAction func resetButton(_ sender: Any) {
+		resetDate()
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		resetDate()
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+
 	}
-
-
+	
+	
+	func resetDate() -> (Void) {
+		dateLabel.text = ""
+		monthTextField.text = ""
+		dayTextField.text = ""
+		yearTextField.text = ""
+		addSecondsTextField.text = ""
+	}
+	
 }
+
 
